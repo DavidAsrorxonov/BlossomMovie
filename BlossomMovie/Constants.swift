f19d83a0461b4a23b01c8c14e361d5c410b6bf72
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-21 18:16:06 +0900

    Add more constants for icons to keep the consistency and for good code practice. Replace hardcoded icon strings with constant strings

--- BlossomMovie/Constants.swift
+++ BlossomMovie/Constants.swift
@@ -9,7 +9,12 @@ import Foundation
 
 struct Constants {
     static let homeString = "Home"
-    static let upcomintString = "Upcoming"
+    static let upcomingString = "Upcoming"
     static let searchString = "Search"
     static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
 }
