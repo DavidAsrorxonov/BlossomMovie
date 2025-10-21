cd8d8df891b18d9de0f678c80b9638c9577b7990
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-21 18:09:48 +0900

    added some constants

--- BlossomMovie/Constants.swift
+++ BlossomMovie/Constants.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    static let upcomintString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+}
