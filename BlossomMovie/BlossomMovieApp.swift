f4782d4d1313215a3219671b1acc2202ad6a7f5b
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-10-21 17:47:05 +0900

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Dovudxon Asrorxonov on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
